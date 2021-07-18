d4e70e0a9cbe19c729eb13e80c1a124844ae0a9d
cloud <62519659+respecting@users.noreply.github.com>
2021-07-17 23:41:55 -0600

    crazy stuff

--- index.js
+++ index.js
@@ -202,6 +202,7 @@ bot.registerCommand("removerole", (msg, args) => {
         msg.channel.createMessage("The role was successfully removed!")
         bot.createMessage("861084246487203850", `The ${args[1]} role was removed by ${msg.author.username}#${msg.author.discriminator} (${msg.author.id}) to ${user[0].username}#${user[0].discriminator} (${user[0].id}).`)
         let role = JSON.parse(fs.readFileSync("roles.json"))
+        bot.getMessage("855832663994531850", role[user[0].id]).then(msg=>msg.delete());
         delete role[user[0].id]
         fs.writeFileSync("roles.json", JSON.stringify(role));
     })
@@ -227,10 +228,11 @@ bot.registerCommand("assignrole", (msg, args) => {
         user[0].addRole(roles[args[1]], `The ${args[1]} role was assigned by ${msg.author.username}#${msg.author.discriminator} (${msg.author.id}).`)
         msg.channel.createMessage("The role was successfully assigned!")
         bot.createMessage("861084246487203850", `The ${args[1]} role was assigned by ${msg.author.username}#${msg.author.discriminator} (${msg.author.id}) to ${user[0].username}#${user[0].discriminator} (${user[0].id}).`)
-        bot.createMessage("855832663994531850", `<@${user[0].id}> (${user[0].id}).`)
-        let role = JSON.parse(fs.readFileSync("roles.json"))
-        role[user[0].id] = roles[args[1]]
-        fs.writeFileSync("roles.json", JSON.stringify(role));
+        bot.createMessage("855832663994531850", `<@${user[0].id}> (${user[0].id}).`).then(msg => {
+            let role = JSON.parse(fs.readFileSync("roles.json"))
+            role[user[0].id] = msg.id
+            fs.writeFileSync("roles.json", JSON.stringify(role));
+        })
     })
 })
 
